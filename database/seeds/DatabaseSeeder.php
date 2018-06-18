21861419f90005441ab75f227d6375299ed6b3f3
Mark Asare <asaremark.ma@gmail.com>
2018-06-18 20:25:44 +0000

    updated seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,14 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(LeaveLevelsTableSeeder::class);
         $this->call(LeaveStatusesTableSeeder::class);
         $this->call(RolesTableSeeder::class);
-<<<<<<< HEAD
         $this->call(StaffTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(LeaveStatusTableSeeder::class);
-        
-=======
-        $this->call(StaffsTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
->>>>>>> c380fb589c0ec27f25f4962c2c90243f5fef6602
     }
 }
